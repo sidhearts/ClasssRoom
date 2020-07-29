879909084846cf53b30d542ede23d7dea8d14bed
sidhearts <rajsiddharth21@gmail.com>
2020-07-29 18:52:48 +0530

    Updates

---
+++ precache-manifest.9d9d62ebdd1f402a6574e5b67de9bef1.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8d7c3302c7295b34b10ceb9caa7abd1",
+    "url": "/ClassRoom/index.html"
+  },
+  {
+    "revision": "ceb89ce8e2228a3f0cd2",
+    "url": "/ClassRoom/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "73cbdddfe59f5602f911",
+    "url": "/ClassRoom/static/css/main.5d93090f.chunk.css"
+  },
+  {
+    "revision": "ceb89ce8e2228a3f0cd2",
+    "url": "/ClassRoom/static/js/2.4c204afa.chunk.js"
+  },
+  {
+    "revision": "7bfaa32b315fe466d84ab2ae49052d17",
+    "url": "/ClassRoom/static/js/2.4c204afa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "73cbdddfe59f5602f911",
+    "url": "/ClassRoom/static/js/main.f151ea5a.chunk.js"
+  },
+  {
+    "revision": "7f0fe352bcedde279149",
+    "url": "/ClassRoom/static/js/runtime-main.c6f6edb1.js"
+  },
+  {
+    "revision": "2fde86d620d666f8b2a444e0fa08511c",
+    "url": "/ClassRoom/static/media/Class.2fde86d6.jpg"
+  },
+  {
+    "revision": "eae13084a5e5601feb208658e9cb1aa4",
+    "url": "/ClassRoom/static/media/bgImage.eae13084.jpg"
+  },
+  {
+    "revision": "742bccd429239d90a927c961c42a3a2f",
+    "url": "/ClassRoom/static/media/laptop.742bccd4.png"
+  }
+]);
